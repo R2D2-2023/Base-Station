204bc576e40ed57a9978e7ab6adfb54ce14f042e
ErikSchipper-HU <75141135+ErikSchipper-HU@users.noreply.github.com>
2023-03-31 11:46:05 +0200

    Updated the sketch
    Als de robot binnen komt begint de arm naar boven te draaien. Mocht de robot tussen tijds weg gaan stopt de arm.
    
    Met de huidige code zal de arm op en neer bewegen.

--- sketch_mar27b.ino
+++ sketch_mar27b.ino
@@ -1,25 +1,37 @@
 int pin1 = 2;
 int pin2 = 3;
-int pin3 = 5;
+int pin3 = 4;
+int pin4 = 5;
+int motorPin1 = 8;
+int motorPin2 = 9;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(pin1, OUTPUT);
-  pinMode(pin2, OUTPUT);
+  pinMode(pin1, INPUT);
+  pinMode(pin2, INPUT);
   pinMode(pin3, INPUT);
+  pinMode(pin4, INPUT);
+
+  pinMode(motorPin1, OUTPUT);  
+  pinMode(motorPin2, OUTPUT);
 }
 
 void loop() {
-
-  // put your main code here, to run repeatedly:
-  // Motor draait de ene kant op.
-if(!digitalRead(pin3)){
-    digitalWrite(pin1, HIGH);
-    digitalWrite(pin2, HIGH);
-    }else{
-      digitalWrite(pin1, HIGH);
-      digitalWrite(pin2, LOW);
-    // motor stopt me}
-}
-}
+  if(!digitalRead(pin4)){
+      if(!digitalRead(pin1)){
+        digitalWrite(motorPin1, HIGH);
+        digitalWrite(motorPin2, LOW);
+        //up
+      }
+      if(!digitalRead(pin2)){
+        digitalWrite(motorPin1, LOW);
+        digitalWrite(motorPin2, HIGH);
+        //down
+        }
+        }else{
+          //stop if robot leaves
+        digitalWrite(motorPin1, HIGH);
+        digitalWrite(motorPin2,HIGH);
+          }
+    }
