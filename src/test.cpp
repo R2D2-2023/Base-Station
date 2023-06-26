826a99320b7d80c7f15f23947c2a182a75f09873
ErikSchipper-HU <75141135+ErikSchipper-HU@users.noreply.github.com>
2023-06-26 11:40:19 +0200

    a new version of the full code
    it now has 2 working loadcells and the lightsensors have been added. Only the chargeboard should be added.

--- src/test.cpp
+++ src/test.cpp
@@ -4,9 +4,12 @@
 #include <iostream>
 #include "HX711.h"
 
-int clock_pin = 0;
+int clock_pin = 9;
 int data_pin1 = 2;
 int data_pin2 = 3;
+
+int light_sensor1 = 25;
+int light_sensor2 = 24;
 
 int hall_sensor_1 = 4;
 int hall_sensor_2 = 5;
@@ -20,14 +23,17 @@ HX711 scale_2(clock_pin, data_pin2);
 
 bool motor1_aan = 0;
 bool motor2_aan = 1;
-bool sensor_detected = 0;
+bool hall_sensor_detected = 0;
+
+bool light_sensor1_detected = 0;
+bool light_sensor2_detected = 0;
 
 int main() {
-    wiringPiSetup();
-    std::cout << "Hello, World!" << std::endl;
-    scale_1.start(66483);// this works to calabrate the scale
-    scale_2.start(66483);//  ^^
-    pinMode(hall_sensor_1, INPUT);
+   	wiringPiSetup();
+    	std::cout << "Hello, World!" << std::endl;
+    	scale_1.start(1058);// this works to calabrate the scale
+    	scale_2.start(-185);//  ^^
+    	pinMode(hall_sensor_1, INPUT);
 	pinMode(hall_sensor_2, INPUT);
 	pinMode(motor_pin_1, OUTPUT);
 	pinMode(motor_pin_2, OUTPUT);
@@ -49,10 +55,22 @@ int main() {
 		//delay(3000);
 	//}
         int grams1 = scale_1.getAvg(2);
-	int grams2 = scale_2.getAvg(2);
-        std::cout << "avg1:" << grams1 << std::endl;
-	std::cout << "avg2:" << grams2 << std::endl;
-        if (grams1 <= -1500 || grams2 <= -1500){
+		int grams2 = scale_2.getAvg(2);
+		std::cout << "avg1:" << grams1 << "\t";
+		std::cout << "avg2:" << grams2 << std::endl;
+		
+		if (digitalRead(light_sensor1)){
+			//robot started entering
+			light_sensor1_detected = true;
+			std::cout << "Sensor1\n";
+		}
+		if (digitalRead(light_sensor2)){
+			//robot has entered the basestation. The arm can go down now. 
+			light_sensor2_detected = true;
+			std::cout << "Sensor2\n";
+		}
+		
+	    if (grams1 >= 4000 || grams2 <= 2000){
 			digitalWrite(motor_pin_1, LOW);
 			digitalWrite(motor_pin_2, LOW);
 			std::cout << "Opladen + schrijven" << std::endl;
@@ -65,29 +83,39 @@ int main() {
 			motor2_aan = !motor2_aan;
 			digitalWrite(motor_pin_1, motor1_aan) ;
 			digitalWrite(motor_pin_2, motor2_aan) ;
-			sensor_detected = 0;
+			hall_sensor_detected = 0;
 		}
+		if(light_sensor1_detected && light_sensor2_detected){
+			// robot has entered and is in position.
+			digitalWrite(motor_pin_1, motor1_aan);
+			digitalWrite(motor_pin_2, motor2_aan);
+			std::cout << "Robot will move down.\n";
+		}
+		
 		if (!digitalRead(hall_sensor_1)){
-			if (sensor_detected != 1){
-				std::cout << "Bottom" << std::endl;
+			// arm is full retracted
+			if (hall_sensor_detected != 1){
+				//std::cout << "retracted" << std::endl;
 				motor1_aan = !motor1_aan; 
 				motor2_aan = !motor2_aan;
 				digitalWrite(motor_pin_1, motor1_aan) ;
 				digitalWrite(motor_pin_2, motor2_aan) ;
-				}
-			sensor_detected = 1;
+			}
+		hall_sensor_detected = 1;
 		}
 		else if (!digitalRead(hall_sensor_2)){
-			if (sensor_detected != 0){
-				std::cout << "Top" << std::endl;
+			if (hall_sensor_detected != 0){
+				//arm is fully extended
+				//std::cout << "extended" << std::endl;
 				motor1_aan = !motor1_aan;
 				motor2_aan = !motor2_aan;
 				digitalWrite(motor_pin_1, motor1_aan) ;
 				digitalWrite(motor_pin_2, motor2_aan) ;
 			}
-			sensor_detected = 0;
+		hall_sensor_detected = 0;
 		}
-		delay(100);
+		
+	delay(100);
 	}
 
     return 0;
